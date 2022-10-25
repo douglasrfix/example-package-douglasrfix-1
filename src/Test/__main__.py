1f96810c30961b3909073968e679b58c68d5c92a
douglasrfix <douglasrfix@prairieobserver.com>
2022-10-24 19:34:14 -0500

    bumped to version a4

--- src/Test/__main__.py
+++ src/Test/__main__.py
@@ -1,16 +1,18 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, an
+import sys
+from Test import hello
+def main():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    hello  # Press Ctrl+F8 to toggle the breakpoint.
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
